092e51244f3c33ca6300c3e33031c4290bcd54d4
Silent <perovskiboki@mail.com>
2022-01-28 14:57:51 +0100

    settings

---
+++ src/main/java/tsp/smartplugin/Settings.java
@@ -0,0 +1,62 @@
+package tsp.smartplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import tsp.smartplugin.event.EntityDamageByEntityListener;
+import tsp.smartplugin.server.Log;
+import tsp.smartplugin.utils.Patterns;
+
+public class Settings {
+
+    private final JavaPlugin plugin;
+    private String pluginName;
+    private boolean compilePatterns = true;
+    private boolean callLivingEntityEvent = true;
+
+    public Settings(JavaPlugin plugin) {
+        this.plugin = plugin;
+        Log.setName(pluginName = plugin.getName());
+    }
+
+    public void load() {
+        Log.setName(pluginName);
+        if (compilePatterns) {
+            Patterns.compile();
+        }
+        if (callLivingEntityEvent) {
+            plugin.getServer().getPluginManager().registerEvents(new EntityDamageByEntityListener(), plugin);
+        }
+    }
+
+    public JavaPlugin getPlugin() {
+        return plugin;
+    }
+
+    public void setPluginName(String pluginName) {
+        this.pluginName = pluginName;
+    }
+
+    public String getPluginName() {
+        return pluginName;
+    }
+
+    /**
+     * Set if patterns should be compiled on startup by default.
+     *
+     * @param compilePatterns if the plugin should compile patterns by default
+     */
+    public void setCompilePatterns(boolean compilePatterns) {
+        this.compilePatterns = compilePatterns;
+    }
+
+    public boolean shouldCompilePatterns() {
+        return compilePatterns;
+    }
+
+    public boolean isCallLivingEntityEvent() {
+        return callLivingEntityEvent;
+    }
+
+    public void setCallLivingEntityEvent(boolean callLivingEntityEvent) {
+        this.callLivingEntityEvent = callLivingEntityEvent;
+    }
+}
