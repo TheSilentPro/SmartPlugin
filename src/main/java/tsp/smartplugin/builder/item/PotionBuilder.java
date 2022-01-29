0901e7836a71d555157f0cd5a76333dcdf3801b5
Silent <perovskiboki@mail.com>
2022-01-29 20:03:31 +0100

    builders

---
+++ src/main/java/tsp/smartplugin/builder/item/PotionBuilder.java
@@ -0,0 +1,72 @@
+package tsp.smartplugin.builder.item;
+
+import org.bukkit.Color;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.PotionMeta;
+import org.bukkit.potion.PotionData;
+import org.bukkit.potion.PotionEffect;
+
+import javax.annotation.Nonnull;
+import java.util.List;
+import java.util.Map;
+
+public class PotionBuilder extends ItemBuilder {
+
+    private final PotionMeta potionMeta;
+
+    public PotionBuilder(@Nonnull ItemStack item) {
+        super(item);
+        potionMeta = (PotionMeta) item.getItemMeta();
+    }
+
+    public PotionBuilder() {
+        this(new ItemStack(Material.POTION));
+    }
+
+    public PotionBuilder color(Color color) {
+        potionMeta.setColor(color);
+        return this;
+    }
+
+    public PotionBuilder basePotionData(PotionData potionData) {
+        potionMeta.setBasePotionData(potionData);
+        return this;
+    }
+
+    public PotionBuilder addEffect(PotionEffect effect, boolean overwrite) {
+        potionMeta.addCustomEffect(effect, overwrite);
+        return this;
+    }
+
+    public PotionBuilder addEffect(PotionEffect effect) {
+        potionMeta.addCustomEffect(effect, true);
+        return this;
+    }
+
+    public PotionBuilder addEffects(boolean overwrite, PotionEffect... effects) {
+        for (PotionEffect effect : effects) {
+            potionMeta.addCustomEffect(effect, overwrite);
+        }
+        return this;
+    }
+
+    public PotionBuilder addEffects(PotionEffect... effects) {
+        addEffects(true, effects);
+        return this;
+    }
+
+    public PotionBuilder addEffects(Map<PotionEffect, Boolean> effects) {
+        for (Map.Entry<PotionEffect, Boolean> entry : effects.entrySet()) {
+            addEffect(entry.getKey(), entry.getValue());
+        }
+        return this;
+    }
+
+    @Override
+    public ItemStack build() {
+        setItemMeta(potionMeta);
+        return super.build();
+    }
+
+}
