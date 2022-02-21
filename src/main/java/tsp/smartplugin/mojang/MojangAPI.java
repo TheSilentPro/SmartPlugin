e3c64912e366bf2fbf877a6abae814d7c79dcda0
Silent <perovskiboki@mail.com>
2022-02-21 22:35:54 +0100

    refactoring

--- src/main/java/tsp/smartplugin/mojang/MojangAPI.java
+++ src/main/java/tsp/smartplugin/mojang/MojangAPI.java
@@ -1,11 +1,14 @@
 package tsp.smartplugin.mojang;
 
 import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitScheduler;
 import tsp.smartplugin.SmartPlugin;
+import tsp.smartplugin.player.info.NameHistory;
+import tsp.smartplugin.player.info.SkinInfo;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -13,7 +16,9 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 import java.util.function.Consumer;
 
@@ -32,7 +37,71 @@ public class MojangAPI {
         this.scheduler = plugin.getServer().getScheduler();
     }
 
-    public void getUniqueId(String name, int timeout, Consumer<JsonObject> json) {
+    /**
+     * Retrieve the unique id of a player based on their name
+     *
+     * @param name The player name
+     * @param timeout Connection timeout
+     */
+    public void getUniqueId(String name, int timeout, Consumer<UUID> uuid) {
+        getUniqueIdJson(name, timeout, json -> {
+            uuid.accept(UUID.fromString(json.get("id").toString()));
+        });
+    }
+
+    public void getUniqueId(String name, Consumer<UUID> uuid) {
+        getUniqueId(name, 5000, uuid);
+    }
+
+    /**
+     * Retrieve skin information about a {@link UUID}
+     *
+     * @param uuid The unique id to check
+     * @param timeout Connection timeout
+     */
+    public void getSkinInfo(UUID uuid, int timeout, Consumer<SkinInfo> skinInfo){
+        getSkinInfoJson(uuid, timeout, json -> {
+            JsonArray properties = json.get("properties").getAsJsonArray();
+            JsonObject textures = properties.get(0).getAsJsonObject();
+
+            String id = json.get("id").toString();
+            String name = json.get("name").toString();
+            String value = textures.get("value").toString();
+            String signature = textures.get("signature").toString();
+            skinInfo.accept(new SkinInfo(id, name, value, signature));
+        });
+    }
+
+    public void getSkinInfo(UUID uuid, Consumer<SkinInfo> skinInfo) {
+        getSkinInfo(uuid, 5000, skinInfo);
+    }
+
+    /**
+     * Retrieve name history of a {@link UUID}
+     *
+     * @param uuid The unique id
+     * @param timeout Connection timeout
+     */
+    public void getNameHistory(UUID uuid, int timeout, Consumer<NameHistory> nameHistory) {
+        getNameHistoryJson(uuid, timeout, json -> {
+            Map<String, Long> history = new HashMap<>();
+            for (JsonElement e : json) {
+                JsonObject obj = e.getAsJsonObject();
+                String name = obj.get("name").toString();
+                long timestamp = obj.get("changedToAt") != null ? obj.get("changedToAt").getAsLong() : -1;
+                history.put(name, timestamp);
+            }
+
+            nameHistory.accept(new NameHistory(uuid, history));
+        });
+    }
+
+    public void getNameHistory(UUID uuid, Consumer<NameHistory> nameHistory) {
+        getNameHistory(uuid, 5000, nameHistory);
+    }
+
+    // Json
+    public void getUniqueIdJson(String name, int timeout, Consumer<JsonObject> json) {
         scheduler.runTaskAsynchronously(plugin, () -> {
             try {
                 String req = "https://api.mojang.com/users/profiles/minecraft/" + name;
@@ -54,11 +123,11 @@ public class MojangAPI {
         });
     }
 
-    public void getUniqueId(String name, Consumer<JsonObject> json) {
-        getUniqueId(name, 5000, json);
+    public void getUniqueIdJson(String name, Consumer<JsonObject> json) {
+        getUniqueIdJson(name, 5000, json);
     }
 
-    public void getSkinInfo(UUID uuid, int timeout, Consumer<JsonObject> json) {
+    public void getSkinInfoJson(UUID uuid, int timeout, Consumer<JsonObject> json) {
         scheduler.runTaskAsynchronously(plugin, () -> {
             try {
                 String req = "https://sessionserver.mojang.com/session/minecraft/profile/" + uuid.toString().replace("-", "") + "?unsigned=false";
@@ -80,11 +149,11 @@ public class MojangAPI {
         });
     }
 
-    public void getSkinInfo(UUID uuid, Consumer<JsonObject> json) {
-        getSkinInfo(uuid, 5000, json);
+    public void getSkinInfoJson(UUID uuid, Consumer<JsonObject> json) {
+        getSkinInfoJson(uuid, 5000, json);
     }
 
-    public void getNameHistory(UUID uuid, int timeout, Consumer<JsonArray> json) {
+    public void getNameHistoryJson(UUID uuid, int timeout, Consumer<JsonArray> json) {
         scheduler.runTaskAsynchronously(plugin, () -> {
             try {
                 String req = "https://api.mojang.com/user/profiles/" + uuid.toString().replace("-", "") + "/names";
@@ -106,11 +175,11 @@ public class MojangAPI {
         });
     }
 
-    public void getNameHistory(UUID uuid, Consumer<JsonArray> json) {
-        getNameHistory(uuid, 5000, json);
+    public void getNameHistoryJson(UUID uuid, Consumer<JsonArray> json) {
+        getNameHistoryJson(uuid, 5000, json);
     }
 
-    public void getServiceStatus(int timeout, Consumer<JsonArray> json) {
+    public void getServiceStatusJson(int timeout, Consumer<JsonArray> json) {
         scheduler.runTaskAsynchronously(plugin, () -> {
             try {
                 String req = "https://status.mojang.com/check";
@@ -132,11 +201,11 @@ public class MojangAPI {
         });
     }
 
-    public void getServiceStatus(Consumer<JsonArray> json) {
-        getServiceStatus(5000, json);
+    public void getServiceStatusJson(Consumer<JsonArray> json) {
+        getServiceStatusJson(5000, json);
     }
 
-    public void getBlockedServers(int timeout, Consumer<List<String>> list) {
+    public void getBlockedServersJson(int timeout, Consumer<List<String>> list) {
         scheduler.runTaskAsynchronously(plugin, () -> {
             try {
                 String req = "https://sessionserver.mojang.com/blockedservers";
@@ -158,8 +227,8 @@ public class MojangAPI {
         });
     }
 
-    public void getBlockedServers(Consumer<List<String>> list) {
-        getBlockedServers(5000, list);
+    public void getBlockedServersJson(Consumer<List<String>> list) {
+        getBlockedServersJson(5000, list);
     }
 
 }
--- src/main/java/tsp/smartplugin/player/PlayerUtils.java
+++ src/main/java/tsp/smartplugin/player/PlayerUtils.java
@@ -1,25 +1,12 @@
 package tsp.smartplugin.player;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import tsp.smartplugin.SmartPlugin;
-import tsp.smartplugin.mojang.MojangAPI;
-import tsp.smartplugin.player.info.NameHistory;
-import tsp.smartplugin.player.info.PlayerInfo;
-import tsp.smartplugin.player.info.SkinInfo;
 import tsp.smartplugin.server.ServerVersion;
 import tsp.smartplugin.utils.StringUtils;
 
 import javax.annotation.Nullable;
-import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
-import java.util.function.Consumer;
 import java.util.function.UnaryOperator;
 
 /**
@@ -83,78 +70,6 @@ public final class PlayerUtils {
             e.printStackTrace();
             return -1;
         }
-    }
-
-    /**
-     * Retrieve information about a {@link UUID} from Mojang
-     *
-     * @param uuid The uuid to check
-     */
-    public static void getPlayerInfo(UUID uuid, Consumer<PlayerInfo> playerInfo) {
-        // TODO: finsih this brain melted
-    }
-
-    /**
-     * Retrieve the unique id of a player based on their name
-     *
-     * @param name The player name
-     * @param timeout Connection timeout
-     */
-    public static void getUniqueId(String name, int timeout, Consumer<UUID> uuid) {
-        SmartPlugin.getInstance().getMojangAPI().getUniqueId(name, timeout, json -> {
-            uuid.accept(UUID.fromString(json.get("id").toString()));
-        });
-    }
-
-    public static void getUniqueId(String name, Consumer<UUID> uuid) {
-        getUniqueId(name, 5000, uuid);
-    }
-
-    /**
-     * Retrieve skin information about a {@link UUID}
-     *
-     * @param uuid The unique id to check
-     * @param timeout Connection timeout
-     */
-    public static void getSkinInfo(UUID uuid, int timeout, Consumer<SkinInfo> skinInfo){
-        SmartPlugin.getInstance().getMojangAPI().getSkinInfo(uuid, timeout, json -> {
-            JsonArray properties = json.get("properties").getAsJsonArray();
-            JsonObject textures = properties.get(0).getAsJsonObject();
-
-            String id = json.get("id").toString();
-            String name = json.get("name").toString();
-            String value = textures.get("value").toString();
-            String signature = textures.get("signature").toString();
-            skinInfo.accept(new SkinInfo(id, name, value, signature));
-        });
-    }
-
-    public static void getSkinInfo(UUID uuid, Consumer<SkinInfo> skinInfo) {
-        getSkinInfo(uuid, 5000, skinInfo);
-    }
-
-    /**
-     * Retrieve name history of a {@link UUID}
-     *
-     * @param uuid The unique id
-     * @param timeout Connection timeout
-     */
-    public static void getNameHistory(UUID uuid, int timeout, Consumer<NameHistory> nameHistory) {
-        SmartPlugin.getInstance().getMojangAPI().getNameHistory(uuid, timeout, json -> {
-            Map<String, Long> history = new HashMap<>();
-            for (JsonElement e : json) {
-                JsonObject obj = e.getAsJsonObject();
-                String name = obj.get("name").toString();
-                long timestamp = obj.get("changedToAt") != null ? obj.get("changedToAt").getAsLong() : -1;
-                history.put(name, timestamp);
-            }
-
-            nameHistory.accept(new NameHistory(uuid, history));
-        });
-    }
-
-    public static void getNameHistory(UUID uuid, Consumer<NameHistory> nameHistory) {
-        getNameHistory(uuid, 5000, nameHistory);
     }
 
 }
